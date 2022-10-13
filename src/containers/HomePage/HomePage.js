73a6cd0be230b9ded457094bfa27367df3a2d869
fi.x64 <hohoangphia@gmail.com>
2022-10-14 00:52:36 +0700

    Add Slicker-Carousel and CustomScrollbars

--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
-import Header from './HomeHeader';
 import HomeHeader from './HomeHeader';
+import Specialty from './Section/Specialty';
 
 class HomePage extends Component {
 
@@ -10,6 +10,8 @@ class HomePage extends Component {
         return (
         <div>
            <HomeHeader />
+           <Specialty />
+           <div style={{ height: '300px' }}></div>
         </div>
         )
     }
