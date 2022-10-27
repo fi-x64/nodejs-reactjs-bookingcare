ed3c32f6a5beb036a33095c5c43ac91cea1386c4
fi.x64 <hohoangphia@gmail.com>
2022-10-27 22:42:56 +0700

    Add authentication user's role to website

--- src/containers/App.js
+++ src/containers/App.js
@@ -14,6 +14,7 @@ import ConfirmModal from '../components/ConfirmModal';
 import HomePage from './HomePage/HomePage.js';
 import CustomScrollbars from '../components/CustomScrollbars';
 import DetailDoctor from './Patient/Doctor/DetailDoctor';
+import Doctor from '../routes/Doctor';
 
 class App extends Component {
 
@@ -47,18 +48,13 @@ class App extends Component {
                                     <Route path={path.HOME} exact component={(Home)} />
                                     <Route path={path.LOGIN} component={userIsNotAuthenticated(Login)} />
                                     <Route path={path.SYSTEM} component={userIsAuthenticated(System)} />
+                                    <Route path={'/doctor/'} component={userIsAuthenticated(Doctor)} />
+
                                     <Route path={path.HOMEPAGE} component={HomePage} />
-                                    <Route path={path.DETAIL_DOCTOR} component={DetailDoctor}/>
+                                    <Route path={path.DETAIL_DOCTOR} component={DetailDoctor} />
                                 </Switch>
                             </CustomScrollbars>
                         </div>
-
-                        {/* <ToastContainer
-                            className="toast-container" toastClassName="toast-item" bodyClassName="toast-item-body"
-                            autoClose={false} hideProgressBar={true} pauseOnHover={false}
-                            pauseOnFocusLoss={true} closeOnClick={false} draggable={false}
-                            closeButton={<CustomToastCloseButton />}
-                        /> */}
 
                         <ToastContainer
                             position="top-right"
--- src/containers/Header/menuApp.js
+++ src/containers/Header/menuApp.js
@@ -5,7 +5,7 @@ export const adminMenu = [
             { name: 'menu.admin.crud-redux', link: '/system/user-redux' },
             { name: 'menu.admin.manage-doctor', link: '/system/manage-doctor' },
             { name: 'menu.admin.manage-admin', link: '/system/user-admin' },
-            // { name: 'menu.system.system-parameter.header', link: '/system/system-parameter' },
+            { name: 'menu.doctor.manage-schedule', link: '/doctor/manage-schedule' }
         ]
     },
     { //quản lý phòng khám
@@ -22,5 +22,15 @@ export const adminMenu = [
         name: 'menu.admin.handbook', menus: [
             { name: 'menu.admin.manage-handbook', link: '/system/manage-handbook' },
         ]
+    },
+];
+
+export const doctorMenu = [
+    { //quản lý người dùng
+        name: 'menu.admin.manage-user', menus: [
+            {
+                name: 'menu.doctor.manage-schedule', link: '/doctor/manage-schedule'
+            }
+        ],
     },
 ];
