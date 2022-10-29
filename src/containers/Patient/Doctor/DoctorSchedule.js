5cc00d2db60c99a12653891570bec21b1540d46b
fi.x64 <hohoangphia@gmail.com>
2022-10-30 01:45:03 +0700

    Edit DoctorSchedule page

--- src/containers/Patient/Doctor/DoctorSchedule.js
+++ src/containers/Patient/Doctor/DoctorSchedule.js
@@ -3,7 +3,7 @@ import { connect } from "react-redux";
 import './DoctorSchedule.scss'
 import { LANGUAGES } from '../../../utils'
 import moment from 'moment';
-import localization from 'moment/locale/vi'
+// import localization from 'moment/locale/vi'
 import { getScheduleDoctorByDate } from '../../../services/userService';
 
 class DoctorSchedule extends Component {
@@ -11,6 +11,7 @@ class DoctorSchedule extends Component {
         super(props);
         this.state = {
             allDays: [],
+            allAvailableTime: [],
         }
     }
 
@@ -23,12 +24,17 @@ class DoctorSchedule extends Component {
         this.setArrayDays(language)
     }
 
+    capitaliizeFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
     setArrayDays = (language) => {
         let allDays = [];
         for (let i = 0; i < 7; i++) {
             let object = {};
             if (language === LANGUAGES.VI) {
-                object.label = moment(new Date()).add(i, 'days').format('dddd - DD/MM');
+                let labelVi = moment(new Date()).add(i, 'days').format('dddd - DD/MM')
+                object.label = this.capitaliizeFirstLetter(labelVi);
             } else {
                 object.label = moment(new Date()).add(i, 'days').locale('en').format('dddd - DD/MM');
             }
@@ -53,29 +59,48 @@ class DoctorSchedule extends Component {
         if (this.props.doctorIdFromParent && this.props.doctorIdFromParent !== -1) {
             let doctorId = this.props.doctorIdFromParent;
             let date = event.target.value;
-            let res = await getScheduleDoctorByDate(doctorId, date);
-            console.log('Check res schedule from react: ', res);
+            let resData = await getScheduleDoctorByDate(doctorId, date);
+            let res = resData.infor;
+            if (res && res.errCode === 0) {
+                this.setState({
+                    allAvailableTime: res.data ? res.data : []
+                })
+            }
+
+            console.log('Check allavailableTime: ', res);
         }
         console.log('Event onchange date value: ', event.target.value);
     }
 
     render() {
-        let { allDays } = this.state;
-
+        let { allDays, allAvailableTime } = this.state;
+        let { language } = this.props;
         return (
             <div className='doctor-schedule-container'>
                 <div className='all-schedule'>
                     <select onChange={(event) => this.handleOnChangeSelect(event)}>
                         {allDays && allDays.length > 0 && allDays.map((item, index) => {
                             return (
-                                <option key={item.value} value={item.value}>{item.label}</option>
+                                <option key={index} value={item.value}>{item.label}</option>
                             )
                         })}
 
                     </select>
                 </div>
                 <div className='all-available-time'>
-
+                    <div className='text-calendar'>
+                        <i className='fas fa-calendar-alt'><span>Lịch khám</span></i>
+                    </div>
+                    <div className='time-content'>
+                        {allAvailableTime && allAvailableTime.length > 0 ? allAvailableTime.map((item, index) => {
+                            let timeDisplay = language === LANGUAGES.VI ? item.timeTypeData.valueVi : item.timeTypeData.valueEn;
+                            return (
+                                <button key={index}>{timeDisplay}</button>
+                            )
+                        })
+                            : <div>Không có lịch hẹn trong thời gian này, vui lòng chọn thời gian khác</div>
+                        }
+                    </div>
                 </div>
             </div>
         );
